167548155fc27e8cb0a393d1e4bc22b403c16ca5
RoySegall <roy@segall.io>
2020-03-24 23:42:08 +0200

    Adding dummy entires

--- routes/blog.py
+++ routes/blog.py
@@ -3,7 +3,16 @@ from rest_framework.views import APIView
 from decorated_router.api.decorators import url_decoration
 
 
-@url_decoration(path="api/blog/")
+@url_decoration(path="api/blogs", name="blogs", extra={'show_title': '🍕'})
+class Blogs(APIView):
+    def get(self, request, show_title):
+        return JsonResponse({'blogs': [
+            {'id': 1, 'title': 'Nice Blog'},
+            {'id': 2, 'title': show_title},
+        ]})
+
+
+@url_decoration(path="api/blog/<blog_id>", name="blog")
 class Blog(APIView):
-    def get(self, request):
-        return JsonResponse({'foo': 'bar'})
+    def get(self, request, blog_id):
+        return JsonResponse({'id': 1, 'title': f'Nice Blog {blog_id}'})
